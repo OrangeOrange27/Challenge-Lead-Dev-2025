647ec4fff78c8dd9295308171e9956d579be29af
Dan <danildanilchenko.27@gmail.com>
2025-11-08 14:07:22 +0100

    added hub bottom panel with animations

--- unity-client/Assets/Core/Hub/UI/Components/Hub/HubBottomPanel.cs
+++ unity-client/Assets/Core/Hub/UI/Components/Hub/HubBottomPanel.cs
@@ -1,11 +1,41 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System;
+using UnityEngine;
 
 namespace Core.Hub.UI
 {
     public class HubBottomPanel : MonoBehaviour
     {
-        [SerializeField] private Button _homeButton;
-        [SerializeField] private Button _resultsButton;
+        [SerializeField] private HubBottomPanelTabView _homeButton;
+        [SerializeField] private HubBottomPanelTabView _resultsButton;
+        
+        public event Action<int> OnTabSelected; //todo: replace int with enum
+
+        private void Awake()
+        {
+            _homeButton.Clicked += OnHomeButtonClicked;
+            _resultsButton.Clicked += OnResultsButtonClicked;
+        }
+
+        private void OnResultsButtonClicked()
+        {
+            _homeButton.SetSelected(false);
+            _resultsButton.SetSelected(true);
+            
+            OnTabSelected?.Invoke(1);
+        }
+
+        private void OnHomeButtonClicked()
+        {
+            _resultsButton.SetSelected(false);
+            _homeButton.SetSelected(true);
+            
+            OnTabSelected?.Invoke(0);
+        }
+
+        private void OnDestroy()
+        {
+            _homeButton.Clicked -= OnHomeButtonClicked;
+            _resultsButton.Clicked -= OnResultsButtonClicked;
+        }
     }
 }
--- unity-client/Assets/Core/Hub/UI/Components/Hub/HubBottomPanelTabView.cs
+++ unity-client/Assets/Core/Hub/UI/Components/Hub/HubBottomPanelTabView.cs
@@ -1,9 +1,63 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Core.Hub.UI
 {
     public class HubBottomPanelTabView : MonoBehaviour
     {
-        
+        [SerializeField] private Button _button;
+
+        [SerializeField] private CanvasGroup _selectedPanel;
+
+        private CancellationTokenSource _cts = new();
+
+        public event Action Clicked
+        {
+            add => _button.onClick.AddListener(value.Invoke);
+            remove => _button.onClick.RemoveListener(value.Invoke);
+        }
+
+        public void SetSelected(bool isSelected)
+        {
+            _button.interactable = !isSelected;
+            _selectedPanel.gameObject.SetActive(isSelected);
+
+            if (!isSelected)
+            {
+                _cts.Cancel();
+            }
+            else
+            {
+                _cts = new CancellationTokenSource();
+                PlaySelectAnimation(_cts.Token).Forget();
+            }
+        }
+
+        private async UniTask PlaySelectAnimation(CancellationToken token)
+        {
+            var originalPos = _selectedPanel.transform.localPosition;
+
+            // Move panel below the screen
+            _selectedPanel.transform.localPosition = originalPos + new Vector3(0, -300f, 0); // adjust offset as needed
+            _selectedPanel.alpha = 0f;
+            _selectedPanel.gameObject.SetActive(true);
+
+            // Animate slide + fade simultaneously
+            var moveTween = _selectedPanel.transform
+                .DOLocalMove(originalPos, 0.25f)
+                .SetEase(Ease.OutCubic);
+
+            var fadeTween = _selectedPanel.DOFade(1f, 0.25f);
+
+            // Await both animations
+            await UniTask.WhenAll(moveTween.AsyncWaitForCompletion().AsUniTask(),
+                fadeTween.AsyncWaitForCompletion().AsUniTask());
+
+            token.ThrowIfCancellationRequested();
+        }
     }
 }
